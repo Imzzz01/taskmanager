17eea6822a5d0b11b4c07bd06cfa6d7bfda6e1a1
Imzzz01 <cyon2z2j@students.codeinstitute.net>
2025-05-08 19:02:24 +0000

    - add comments to all html and static files.

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,13 +78,9 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'iman',
-        'HOST': 'db',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
