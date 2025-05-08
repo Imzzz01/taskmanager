ad6008aa333354f3d8d08007d8d46474eb41768f
Imzzz01 <cyon2z2j@students.codeinstitute.net>
2025-05-08 18:43:21 +0000

    - connect postgresql with django project - create docker file as subo is not supported with codespace.

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,14 +78,17 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'iman',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
