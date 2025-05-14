bb5470eb0557ed13548af1933ee933e2ce4e3d34
Imzzz01 <cyon2z2j@students.codeinstitute.net>
2025-05-14 20:19:53 +0000

    deploy on railway

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from dotenv import load_dotenv
+load_dotenv()  # Add at the top of settings.py
 
 from pathlib import Path
 import os
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^#*e(g*m6op4@be*n-h-j+@qzkq6*v-xyu8*%vspy^xpuj#xm^"
+SECRET_KEY = os.getenv("SECRET_KEY", "your-default-insecure-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ and 'RAILWAY_ENVIRONMENT' not in os.environ
@@ -87,18 +89,13 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-   if os.environ.get('DATABASE_URL'):
-    # Railway automatically provides the DATABASE_URL environment variable
-    import dj_database_url
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-else:
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
+
 
 
 
